dbbe1323b2043064be1e7106ed0ef46c4d67fb07
jmisoardi <jmisoardi@hotmail.com>
2024-02-11 00:59:35 -0300

    Creación del Header y el Footer

---
+++ bd.php
@@ -0,0 +1,14 @@
+<?php 
+
+    $servidor = "localhost";
+    $baseDeDatos = "app";
+    $usuario = "root";
+    $contrasenia = "";
+
+    try{
+        $conexion = new PDO("mysql:host=$servidor;dbname=$baseDeDatos",$usuario,$contrasenia);
+    }catch (Exception $ex){
+        echo $ex->getMessage();
+    }
+
+?> 
---
+++ templates/header.php
@@ -0,0 +1,51 @@
+
+<!--Contiene Menu y parte del Conteiner -->
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
+
+    <body>
+        <header>
+            <!-- place navbar here -->
+        </header>
+        
+            <!--Esto es la barra de navegacion que incluye Sistema, Personal, Rol, Usuario, Cerrar Sesion-->
+            <nav class="navbar navbar-expand navbar-light bg-light">
+                <ul class="nav navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#" aria-current="page">Sistema<span class="visually-hidden">(current)</span></a
+                        >
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Personal</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Rol</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Usuarios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Cerrar Sesión</a>
+                    </li>
+                </ul>
+            </nav>
+            
+        <main class="container"> 
