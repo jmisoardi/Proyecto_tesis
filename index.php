22a50f060cebf901c579b4ea85b70ca906c797d1
jmisoardi <jmisoardi@hotmail.com>
2024-03-25 11:43:47 -0300

    texto de inicio

--- index.php
+++ index.php
@@ -7,8 +7,9 @@
     <br>
             <div class="p-5 mb-4 bg-light rounded-3">
                 <div class="container-fluid py-5" style="background-color:azure">
-                    <h1 class="display-5 fw-bold">Texto de ejemplo</h1>
+                    <h1 class="display-5 fw-bold">Presentación</h1>
                     <p class="col-md-8 fs-4">
+                    Se ha distinguido desde siempre por tratarse de un proyecto privado  de carácter original y vanguardista, popular, inclusivo, atento a la diversidad, creativo, innovador y participativo.
                         Using a series of utilities, you can create this jumbotron, just
                         like the one in previous versions of Bootstrap. Check out the
                         examples below for how you can remix and restyle it to your liking.
