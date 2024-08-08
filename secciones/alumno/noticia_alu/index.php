67af34274d708f41115e8e580641f3cdf854cc85
jmisoardi <jmisoardi@hotmail.com>
2024-08-08 10:28:41 -0300

    Trabajo de noticia ternimado en Alumno

--- secciones/alumno/noticia_alu/index.php
+++ secciones/alumno/noticia_alu/index.php
@@ -28,9 +28,10 @@
     <meta charset="UTF-8">
     <title>Noticias</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../../css/styles.css">
 </head>
     <body>
-        <!--Estilo para el titulo Personal-->
+        <!--Estilo para el titulo-->
     <style> 
         h1 {
             text-align: center; font-family: Georgia, sans-serif;
@@ -39,62 +40,57 @@
         h2 {
             text-align: center; font-family: Georgia, sans-serif;
         }
+        
         </style>
         <div class="card">
-            <!--Header y button primary-->lightblue
-            <div class="card-header" style="background-color:lightblue">   
-                <label class="card-header" style="background-color:bisque"><h1>Noticias</h1></label>
-            </div>
+            <!--Header y button primary-->
+            <label class="card" style="background-color:gold"><h1>Noticias</h1></label>
+            <div class="card-header" style="background-color:silver"></div>
+        <br>
+        <div class="card-body-lg" style="background-color:azure">
         
-        <div class="card-body-xl" style="background-color:azure">
-        
-        <div class="table-responsive">
-        <!--Usamos el id "tabla_id" para que tengan los estilos de busquedas, el script se encuentra en el footer-->
-            <table class="table" id="tabla_id">
-                <thead>
-                    <tr>
-                        <!--Alineación central del ID, Nombre/Rol, Acciones-->
-                        <style> 
-                        th {
-                            text-align: center; font-family: Georgia, sans-serif;
-                            }
-                        </style>
+            <div class="table-responsive">
+            <!--Usamos el id "tabla_id" para que tengan los estilos de busquedas, el script se encuentra en el footer-->
+                <table class="table" id="tabla_id">
+                    <thead>
+                        <tr>
+                            <!--Alineación central del Fecha, Nombre, Titulo, Cuerpo, Acciones-->
+                            <style> 
+                            th {
+                                text-align: center; font-family: Georgia, sans-serif;
+                                }
+                            </style>
 
-                            <th scope="col" style="background-color:azure"><u>ID</u></th>
-                            <th scope="col" style="background-color:azure"><u>Fecha</u></th>
-                            <th scope="col" style="background-color:azure"><u>Titulo</u></th>
-                            <th scope="col" style="background-color:azure"><u>Cuerpo</u></th> 
-                            <th scope="col" style="background-color:azure"><u>Acciones</u></th>
-                    </tr>
-                </thead>
-                
-                    <tbody>
-                <!--Usamos el foreach para recorrer el arreglo de la lista de persona y asignarlo a la variable $registro-->  
-                        <?php foreach ($lista_tbl_noticia as $registro) {?>     
-                        
-                        <!--Alineación central style-->
-                            <tr class="">
-                                <td scope="row"><?php echo $registro['id'];?></td>
-                                <!--La etiqueta <td> podemos agrupar datos en una sola casilla-->
-                                            
-                                            <td> <?php echo $fecha_formateada = date('d/m/Y H:i:s', strtotime($registro['fecha'])); ?></td> 
-                                            <td> <?php echo $registro['titulo']; ?> </td>
-                                            <td> <?php echo $registro['cuerpo']; ?></td> 
-                                            <!--Etiqueta de botones Editar y Eliminar-->
-                                            <td>
-                                                <!--Utilizamos bs5-button-a seguido de la línea de código para editar el ID de la fila. -->
-                                                <a class="btn btn-info" href="vista_alu.php?txtID=<?php echo $registro['id']; ?>" role="button" >Ver</a >
-                                                <!--Utilizamos bs5-button-a seguido de la línea de código para obtener el ID y que nos elimine la fila. -->
-                                                <!--El signo sirve para pasar parametros por URL.-->
-                                                <a class="btn btn-danger" href="javascript:borrar(<?php echo $registro['id']; ?>);" role="button" >Eliminar</a >   
-                                            </td>
-                            </tr>
-                        <?php } ?>
-                    </tbody>
+                                <!-- <th scope="col" style="background-color:azure"><u>ID</u></th> -->
+                                <th scope="col" style="background-color:azure"><u>Fecha</u></th>
+                                <th scope="col" style="background-color:azure"><u>Titulo</u></th>
+                                <th scope="col" style="background-color:azure"><u>Cuerpo</u></th> 
+                                <th scope="col" style="background-color:azure"><u>Acciones</u></th>
+                        </tr>
+                    </thead>
+                    
+                        <tbody>
+                    <!--Usamos el foreach para recorrer el arreglo de la lista de persona y asignarlo a la variable $registro-->  
+                            <?php foreach ($lista_tbl_noticia as $registro) {?>  
+                                <tr class="">
+                                    <td scope="row"><?php echo $fecha_formateada = date('d/m/Y H:i:s', strtotime($registro['fecha'])); ?></td>
+                                    <!--La etiqueta <td> podemos agrupar datos en una sola casilla-->
+                                                <td> <?php echo $registro['titulo']; ?> </td>
+                                                <td> <?php echo $registro['cuerpo']; ?></td> 
+                                                <!--Etiqueta de botones Editar y Eliminar-->
+                                                <td>
+                                                    <!--Utilizamos bs5-button-a seguido de la línea de código para editar el ID de la fila. -->
+                                                    <a class="btn btn-info" href="vista_alu.php?txtID=<?php echo $registro['id']; ?>" role="button" >Ver</a >
+                                                </td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
                 </table>
             </div>
         </div>
-        <div class="card-footer text-muted" style="background-color:bisque"></div>
+        <br>
+        <br>
+        <div class="card-footer text-muted" style="background-color:silver"></div>
     </div>
 </body>
 </html>
