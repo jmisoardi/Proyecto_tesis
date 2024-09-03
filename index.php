cbb64c588afcc17ec1fe58351da4f1b6cbb53884
Jair Isoardi <jmisoardi@hotmail.com>
2024-09-02 22:22:41 -0300

    Cargamos imagen de fondo login

--- index.php
+++ index.php
@@ -63,9 +63,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
         <!-- Bootstrap CSS v5.2.1 -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"/>
-        <link rel="stylesheet" href="css/styles.css">
+        <link rel="stylesheet" href="css/imagen_tesis/background-login.webp">
     </head>
     <body>
+        <div class="background-login"></div>
+        <div class="content-login">
         <header>
             <head>
                 <!--Estilo css para ver el ojo en el LOGIN-->
--- secciones/alumno/templates_alu/header_alu.php
+++ secciones/alumno/templates_alu/header_alu.php
@@ -18,7 +18,7 @@
         <meta charset="UTF-8">
         <title>Alumno</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">    
-        <link rel="stylesheet" href="../../css/styles.css">
+        <!-- <link rel="stylesheet" href="../../css/styles.css"> -->
         <!--Script para sweet alert -->
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> 
     </head>
