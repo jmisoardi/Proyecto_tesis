c44db06f8b7e103b0365b2f7504215945a4f6cb9
jmisoardi <jmisoardi@hotmail.com>
2024-11-02 15:13:00 -0300

    Correccion en los input de administrador/personal, espacios vacios, utilizamos required en html

--- secciones/administrador/personal/crear.php
+++ secciones/administrador/personal/crear.php
@@ -3,7 +3,7 @@
     include("../../../bd.php");
     
     if ($_POST){
-        print_r($_POST);
+        /* print_r($_POST); */
         
         //Verificamos si existe una peticion $_POST, validamos si ese if isset sucedio, lo vamos igualar a ese valor, de lo contrario no sucedio
         //Lo verificamos a este valor $_POST["usuario"] lo comparamos con la llave de pregunta (?) $_POST["usuario"] si sucedio, de lo contrario va a quedar vacío.
@@ -18,26 +18,30 @@
         $usuario = (isset($_POST["usuario"])) ? $_POST["usuario"]: "";
         $password = (isset($_POST["password"])) ? $_POST["password"]: "";
 
-        //Preparamos la insercción de los datos.
-        $sentencia = $conexion->prepare("INSERT INTO 
-        `tbl_persona`(`id`, `nombre`, `apellido`, `dni`, `fechanacimiento`, `email`, `telefono`, `idrol`, `fechaingreso`, `usuario`, `password` ) 
-        VALUES (null, :nombre, :apellido, :dni, :fechanacimiento, :email, :telefono, :idrol, :fechaingreso, :usuario, :password)");
+        //Verificamos si el campo usuario esta vacio.
+
+
+            //Preparamos la insercción de los datos.
+            $sentencia = $conexion->prepare("INSERT INTO 
+            `tbl_persona`(`id`, `nombre`, `apellido`, `dni`, `fechanacimiento`, `email`, `telefono`, `idrol`, `fechaingreso`, `usuario`, `password` ) 
+            VALUES (null, :nombre, :apellido, :dni, :fechanacimiento, :email, :telefono, :idrol, :fechaingreso, :usuario, :password)");
+            
+            //Asignando los valores que vienen del  método POST (Los que vienen del formulario).
+            $sentencia->bindParam(":nombre",$nombre);
+            $sentencia->bindParam(":apellido",$apellido);
+            $sentencia->bindParam(":dni",$dni);
+            $sentencia->bindParam(":fechanacimiento",$fechanacimiento);
+            $sentencia->bindParam(":email",$email);
+            $sentencia->bindParam(":telefono",$telefono);
+            $sentencia->bindParam(":idrol",$idrol);
+            $sentencia->bindParam(":fechaingreso",$fechaingreso);
+            $sentencia->bindParam(":usuario",$usuario);
+            $sentencia->bindParam(":password",$password);
+            $sentencia->execute();
+            //Mensaje de Registro Agregado (Sweet alert).
+            $mensaje="Registro Agregado";
+            header("Location:index.php?mensaje=".$mensaje);
         
-        //Asignando los valores que vienen del  método POST (Los que vienen del formulario).
-        $sentencia->bindParam(":nombre",$nombre);
-        $sentencia->bindParam(":apellido",$apellido);
-        $sentencia->bindParam(":dni",$dni);
-        $sentencia->bindParam(":fechanacimiento",$fechanacimiento);
-        $sentencia->bindParam(":email",$email);
-        $sentencia->bindParam(":telefono",$telefono);
-        $sentencia->bindParam(":idrol",$idrol);
-        $sentencia->bindParam(":fechaingreso",$fechaingreso);
-        $sentencia->bindParam(":usuario",$usuario);
-        $sentencia->bindParam(":password",$password);
-        $sentencia->execute();
-        //Mensaje de Registro Agregado (Sweet alert).
-        $mensaje="Registro Agregado";
-        header("Location:index.php?mensaje=".$mensaje);
     }
     //Preparamos la sentencia de $conexion y ejecutamos, seguido creamos una lista_tbl_rol, que las filas se devuelvan como un array asociativo.
     $sentencia = $conexion->prepare("SELECT * FROM `tbl_rol`");
@@ -52,7 +56,12 @@
         h1 {
             text-align: center; font-family: Georgia, sans-serif;
         }
+        input:invalid:focus{
+            border: 2px solid red;
+            background-color: #ffdddd;  
+        }
     </style>
+    
         <h1>Datos Personales</h1> 
         <div class="card">
             <div class="card-header" style="background-color:bisque">Ingrese los datos para el registro</div>
@@ -61,27 +70,27 @@
             <form  action="" method="post" enctype="multipart/form-data" style="background-color:azure">
                 <div class="mb-3">
                     <label for="nombre" class="form-label"><u>Nombre:</u></label>
-                    <input type="text" class="form-control" name="nombre" id="nombre" aria-describedby="helpId" placeholder="Ingrese Nombre"/>
+                    <input type="text" class="form-control" name="nombre" id="nombre" aria-describedby="helpId" placeholder="Ingrese Nombre" required/>
                 </div>
                 <div class="mb-3"> 
                     <label for="apellido" class="form-label"><u>Apellido:</u></label>
-                    <input type="text" class="form-control" name="apellido" id="apellido" aria-describedby="helpId" placeholder="Ingrese Apellido"/>
+                    <input type="text" class="form-control" name="apellido" id="apellido" aria-describedby="helpId" placeholder="Ingrese Apellido" required/>
                 </div>
                 <div class="mb-3">
                     <label for="dni" class="form-label"><u>Dni:</u></label>
-                    <input type="number" class="form-control" name="dni" id="dni" aria-describedby="helpId" placeholder="Ingrese Dni"/>            
+                    <input type="number" class="form-control" name="dni" id="dni" aria-describedby="helpId" placeholder="Ingrese Dni"  required/>            
                 </div>
                 <div class="mb-3">
                     <label for="fechanacimiento" class="form-label"><u>Fecha/Nacimiento:</u></label>
-                    <input type="date" class="form-control" name="fechanacimiento" id="fechanacimiento" aria-describedby="helpId" placeholder="Ingrese Dni"/>            
+                    <input type="date" class="form-control" name="fechanacimiento" id="fechanacimiento" aria-describedby="helpId" placeholder="Ingrese Dni" required />            
                 </div>
                 <div class="mb-3">
                     <label for="email" class="form-label"><u>Email:</u></label>
-                    <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelpId" placeholder=" Por ejemplo: abc@mail.com"/> 
+                    <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelpId" placeholder=" Por ejemplo: abc@mail.com" required/> 
                 </div>
                 <div class="mb-3">
                     <label for="telefono" class="form-label"><u>Teléfono:</u></label>
-                    <input type="number" class="form-control" name="telefono" id="telefono" aria-describedby="helpId" placeholder="Ingrese Teléfono"/>            
+                    <input type="number" class="form-control" name="telefono" id="telefono" aria-describedby="helpId" placeholder="Ingrese Teléfono" />            
                     <small id="helpId" class="form-text text-muted"> sin (0) y sin (15)</small>
                 </div>
                 <div class="mb-3">
@@ -97,20 +106,18 @@
                 </div>
                 <div class="mb-3">
                     <label for="fechaingreso" class="form-label"><u>Fecha/Ingreso:</u></label>
-                    <input type="date" class="form-control" name="fechaingreso" id="fechaingreso" aria-describedby="helpId" placeholder="Ingrese Fecha"/>
+                    <input type="date" class="form-control" name="fechaingreso" id="fechaingreso" aria-describedby="helpId" placeholder="Ingrese Fecha" />
                     
                 </div>
                 <div class="mb-3">
                     <label for="usuario" class="form-label"><u>Usuario:</u></label>
-                    <input type="text" class="form-control" name="usuario" id="usuario" aria-describedby="helpId" placeholder="Usuario"/>
-                    
+                    <input type="text" class="form-control" name="usuario" id="usuario" aria-describedby="helpId" placeholder="Usuario" required/>
                 </div>
                 <div class="mb-3">
                     <label for="password" class="form-label"><u>Password:</u></label>
-                    <input type="text" class="form-control" name="password" id="password" aria-describedby="helpId" placeholder="Password"/>
-                    
+                    <input type="text" class="form-control" name="password" id="password" aria-describedby="helpId" placeholder="Password" required/>
                 </div>
-                
+    
                 <!--Button bs5-button-default y bs5-button-a (sirve para direccionar) -->
                 <button type="submit" class="btn btn-success">Agregar</button>
                 <a name="" id="" class="btn btn-primary" href="index.php" role="button" >Cancelar</a>
