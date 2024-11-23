c20e5fc32e6e8ac8ee695232d9df23edf8f311bf
jmisoardi <jmisoardi@hotmail.com>
2024-11-22 21:53:31 -0300

    Trabajando en alumno material

--- secciones/alumno/unidad_alu/index.php
+++ secciones/alumno/unidad_alu/index.php
@@ -1,21 +1,55 @@
+<?php
+    session_start();
+    include("../../../bd.php");
+
+    /* // Verifica si el usuario es un alumno
+    if ($_SESSION['rol'] !== 'alumno') {
+        header("Location: ../../../index.php");
+        exit;
+    } */
+
+    // Consulta para obtener el material habilitado
+    $sentencia = $conexion->prepare("SELECT tbl_tema.titulo, tbl_tema.descripcion, tbl_tema.archivo, tbl_nivel.nombre_nivel
+                                    FROM tbl_tema
+                                    LEFT JOIN tbl_nivel ON tbl_tema.nivel_id = tbl_nivel.id
+                                    WHERE tbl_tema.habilitado = 1");
+    $sentencia->execute();
+    $materiales = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <?php include("../templates_alu/header_alu.php");?>
-<br>
-<!DOCTYPE html>
-    <html lang="es">
-        <head>
-            <meta charset="UTF-8">
-            <title>Unidad</title>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-            <link rel="stylesheet" href="../../../css/styles.css">
-        </head>
-        <body>
-            <div class="container mt-5" style="background-color:azure">
-                <h1>Unidad del Estudiante</h1>
-                <p class="lead">Nombre: [Nombre del Alumno]</p>
-                <p class="lead">Email: [Email del Alumno]</p>
-                <p class="lead">Carrera: [Carrera del Alumno]</p>
-            </div>
-        </body>
-    </html>
+
+<div class="container">
+    <h1 class="text-center mt-4">Materiales Disponibles</h1>
+    <div class="table-responsive mt-4">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Título</th>
+                    <th>Descripción</th>
+                    <th>Nivel</th>
+                    <th>Archivo</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($materiales as $material) { ?>
+                    <tr>
+                        <td><?php echo $material['titulo']; ?></td>
+                        <td><?php echo $material['descripcion']; ?></td>
+                        <td><?php echo $material['nombre_nivel']; ?></td>
+                        <td>
+                            <?php if (!empty($material['archivo'])) { ?>
+                                <a href="/Proyecto_tesis/uploads/<?php echo $material['archivo']; ?>" target="_blank">Descargar</a>
+                            <?php } else { ?>
+                                No disponible
+                            <?php } ?>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+
 <br><br>
 <?php include("../templates_alu/footer_alu.php");?>
